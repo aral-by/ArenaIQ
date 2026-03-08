5dfcfcc8f92eb9b6d404ce8a833bf8d2e84fc257
Aral Büyükyanbol <aral-by@users.noreply.github.com>
2026-03-08 23:18:08 +0300

    icon for extension icon manifest

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -7,6 +7,7 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     rollupOptions: {
